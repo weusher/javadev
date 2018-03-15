6c8f60994dd004eeb32b249c0674363f04aa24e1
Mateusz <sacz.mateusz@gmail.com>
2018-03-15 15:26:18 +0100

    pierwsza

---
+++ src/XmassTree.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class XmassTree {
+    public static void main(String[] args)
+    {
+        int height;
+        System.out.print("xmas tree height: ");
+        Scanner input = new Scanner(System.in);
+        height = input.nextInt();
+        for (int i = 0; i < height; ++i)
+        {
+            for (int j = 0; i < height; ++i)
+            {
+
+            }
+        }
+    }
+}
